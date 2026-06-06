6769dc096c4a400280c4ba616fdce51b27ad7125
Julfikar Ali <julfikarali.282@gmail.com>
2026-06-06 19:42:48 +0600

    Update learners CRUD

--- application/modules/learner/models/Learner_model.php
+++ application/modules/learner/models/Learner_model.php
@@ -13,8 +13,9 @@ class Learner_model extends Fm_model{
     }    
     
     // get total rows
-    function total_rows($q = NULL) {    
+    function total_rows($q = NULL, $batch_id = NULL) {    
         if($q){
+            $this->db->group_start();
         	$this->db->like('id', $q);
 			$this->db->or_like('batch_id', $q);
 			$this->db->or_like('name', $q);
@@ -27,33 +28,53 @@ class Learner_model extends Fm_model{
 			$this->db->or_like('blood_group', $q);
 			$this->db->or_like('is_resident', $q);
 			$this->db->or_like('remarks', $q);
+            $this->db->group_end();
 		}
+
+        if($batch_id){
+            $this->db->where('batch_id', $batch_id);
+        }
+
 		$this->db->from($this->table);
 
         return $this->db->count_all_results();
     }
 
-    // get data with limit and search
-    function get_limit_data($limit, $start = 0, $q = NULL) {
+    // get data by id
+    function get_by_id($id){
+        $this->db->select('learners.*, batches.name as batch_name, districts.bn_name as district_name');
+        $this->db->join('batches', 'batches.id = learners.batch_id', 'left');
+        $this->db->join('districts', 'districts.id = learners.district_id', 'left');
+        $this->db->where('learners.'.$this->id, $id);
+        return $this->db->get($this->table)->row();
+    }
+    function get_limit_data($limit, $start = 0, $q = NULL, $batch_id = NULL) {
         $this->db->select('learners.*, batches.name as batch_name,  districts.bn_name as district_name');
         $this->db->join('batches', 'batches.id = learners.batch_id', 'left');   
         $this->db->join('districts', 'districts.id = learners.district_id', 'left');
 
-        $this->db->order_by($this->id, $this->order);
+        $this->db->order_by('learners.'.$this->id, $this->order);
         if($q){
-        	$this->db->like('id', $q);
-			$this->db->or_like('batch_id', $q);
-			$this->db->or_like('name', $q);
-			$this->db->or_like('dob', $q);
-			$this->db->or_like('nid', $q);
-			$this->db->or_like('father', $q);
-			$this->db->or_like('mother', $q);
-			$this->db->or_like('district_id', $q);
-			$this->db->or_like('primary_mobile', $q);
-			$this->db->or_like('blood_group', $q);
-			$this->db->or_like('is_resident', $q);
-			$this->db->or_like('remarks', $q);
+            $this->db->group_start();
+        	$this->db->like('learners.id', $q);
+			$this->db->or_like('learners.batch_id', $q);
+			$this->db->or_like('learners.name', $q);
+			$this->db->or_like('learners.dob', $q);
+			$this->db->or_like('learners.nid', $q);
+			$this->db->or_like('learners.father', $q);
+			$this->db->or_like('learners.mother', $q);
+			$this->db->or_like('learners.district_id', $q);
+			$this->db->or_like('learners.primary_mobile', $q);
+			$this->db->or_like('learners.blood_group', $q);
+			$this->db->or_like('learners.is_resident', $q);
+			$this->db->or_like('learners.remarks', $q);
+            $this->db->group_end();
 		}
+
+        if($batch_id){
+            $this->db->where('learners.batch_id', $batch_id);
+        }
+
 		$this->db->limit($limit, $start);
         return $this->db->get($this->table)->result();
     }
--- application/modules/learner/views/learner/details.php
+++ application/modules/learner/views/learner/details.php
@@ -18,19 +18,29 @@
             <?php echo $this->session->flashdata('message'); ?>
         </div>
         <table class="table table-striped">
-	    <tr><td width="150">Batch</td><td width="5">:</td><td><?php echo $batch_id; ?></td></tr>
+	    <tr><td width="150">Batch</td><td width="5">:</td><td><?php echo $batch_name; ?></td></tr>
 	    <tr><td width="150">Name</td><td width="5">:</td><td><?php echo $name; ?></td></tr>
-	    <tr><td width="150">Dob</td><td width="5">:</td><td><?php echo $dob; ?></td></tr>
-	    <tr><td width="150">Nid</td><td width="5">:</td><td><?php echo $nid; ?></td></tr>
+	    <tr><td width="150">Date Of Birth</td><td width="5">:</td><td><?php echo $dob; ?></td></tr>
+	    <tr><td width="150">NID/BID</td><td width="5">:</td><td><?php echo $nid; ?></td></tr>
 	    <tr><td width="150">Father</td><td width="5">:</td><td><?php echo $father; ?></td></tr>
 	    <tr><td width="150">Mother</td><td width="5">:</td><td><?php echo $mother; ?></td></tr>
-	    <tr><td width="150">District</td><td width="5">:</td><td><?php echo $district_id; ?></td></tr>
+	    <tr><td width="150">District</td><td width="5">:</td><td><?php echo $district_name; ?></td></tr>
 	    <tr><td width="150">Primary Mobile</td><td width="5">:</td><td><?php echo $primary_mobile; ?></td></tr>
 	    <tr><td width="150">Blood Group</td><td width="5">:</td><td><?php echo $blood_group; ?></td></tr>
 	    <tr><td width="150">Second Contact Person</td><td width="5">:</td><td><?php echo $second_contact_person; ?></td></tr>
 	    <tr><td width="150">Second Contact Mobile</td><td width="5">:</td><td><?php echo $second_contact_mobile; ?></td></tr>
 	    <tr><td width="150">Is Resident</td><td width="5">:</td><td><?php echo $is_resident; ?></td></tr>
-	    <tr><td width="150">Photo</td><td width="5">:</td><td><?php echo $photo; ?></td></tr>
+	    <tr>
+            <td width="150">Photo</td>
+            <td width="5">:</td>
+            <td>
+                <?php if ($photo && file_exists('./uploads/learner/' . $photo)) { ?>
+                    <img src="<?php echo base_url('uploads/learner/' . $photo); ?>" alt="Photo" style="max-width:200px;border-radius:8px;box-shadow:0 2px 10px rgba(0,0,0,0.1);">
+                <?php } else { ?>
+                    <img src="<?php echo base_url('assets/admin/dist/img/avatar.png'); ?>" alt="Default" style="max-width:100px;border-radius:8px;">
+                <?php } ?>
+            </td>
+        </tr>
 	    <tr><td width="150">Remarks</td><td width="5">:</td><td><?php echo $remarks; ?></td></tr>
 	    <tr><td width="150">Created At</td><td width="5">:</td><td><?php echo $created_at; ?></td></tr>
 	    <tr><td width="150">Updated At</td><td width="5">:</td><td><?php echo $updated_at; ?></td></tr>
